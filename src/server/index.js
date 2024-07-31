e689e3eaa6c54bd8e7d6271b78e6f187e686366c
Doug Sparks <dksparks@gmail.com>
2024-07-31 19:48:24 -0400

    add call to nasa api

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,62 @@ app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
+app.get("/rover/:rover", async (req, res) => {
+  const rover = req.params.rover;
+  const apiKey = `api_key=${process.env.API_KEY}`;
+  const urlStem = "https://api.nasa.gov/mars-photos/api/v1";
+
+  // Minimum number of photos to show
+  const minNumberOfPhotosToShow = 60;
+
+  // First get the manifest
+  let manifest;
+  try {
+    manifest = await fetch(`${urlStem}/manifests/${rover}?${apiKey}`)
+      .then((fetchResponse) => fetchResponse.json());
+  } catch (err) {
+    console.log("error:", err);
+    res.send(null);
+  }
+  const { name, landing_date, launch_date, status, photos: photoInfo } =
+    manifest.photo_manifest;
+
+  // Determine which of the most recent sols
+  // we need to look up to obtain at least
+  // minNumberOfPhotosToShow
+  const nPhotos = photoInfo.map((x) => x.total_photos);
+  const nPhotosSince = nPhotos.map((_, i) =>
+    nPhotos.slice(i).reduce((total, next) => total + next)
+  );
+  const startAt = nPhotosSince.findLastIndex((n) =>
+    n >= minNumberOfPhotosToShow
+  );
+  const sols = photoInfo.slice(startAt).map((x) => x.sol);
+
+  // Map each sol to an array of photo objects
+  const photosBySol = await Promise.all(sols.map(async (sol) => {
+    let photoData;
+    try {
+      const params = `${apiKey}&sol=${sol}`;
+      photoData = await fetch(`${urlStem}/rovers/${rover}/photos?${params}`)
+        .then((fetchResponse) => fetchResponse.json());
+    } catch (err) {
+      console.log("error:", err);
+      return [];
+    }
+    return photoData.photos;
+  }));
+  console.log(photosBySol);
+
+  // Send response with info and photos
+  res.send({
+    name,
+    launch_date,
+    landing_date,
+    status,
+    photos: photosBySol.flat(),
+  });
+});
 
 // example API call
 app.get("/apod", async (req, res) => {
